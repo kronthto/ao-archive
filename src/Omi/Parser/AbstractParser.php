fd1423f916bc6bc525fbb88a33d196fe97b941d4
Tobias Kronthaler <kronthto@gmail.com>
2019-01-12 22:57:24 +0100

    Abstract omi binary item parser / RareItem (Fix)

---
+++ src/Omi/Parser/AbstractParser.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Kronthto\AOArchive\Omi\Parser;
+
+use PhpBinaryReader\BinaryReader;
+
+abstract class AbstractParser
+{
+    protected $data = [];
+
+    public function __construct(string $data)
+    {
+        $reader = new BinaryReader($data);
+
+        foreach ($this->getDefinition() as $name => $type) {
+            $this->data[$name] = $this->readValue($reader, $type);
+        }
+    }
+
+    protected function readValue(BinaryReader $reader, $type)
+    {
+        if (\is_int($type)) {
+            $reader->readBytes($type); // discard
+            return null;
+        }
+
+        if (preg_match('/^string(\d+)$/', $type, $matches) === 1) {
+            return trim($reader->readString((int) $matches[1]));
+        }
+
+        switch ($type) {
+            case 'int':
+                return $reader->readInt32();
+            case 'uint':
+                return $reader->readUInt32();
+            case 'byte':
+                return $reader->readInt8();
+            case 'stat':
+                $stat = [];
+                for ($i = 0; $i < 6; ++$i) {
+                    $stat[] = $reader->readInt16();
+                }
+
+                return $stat;
+            case 'short':
+                return $reader->readInt16();
+            case 'float':
+                return unpack('f', $reader->readBytes(4))[1];
+            case 'long':
+                return $reader->readInt64();
+            default:
+                throw new \BadMethodCallException('Not implemented type: ' . $type);
+        }
+    }
+
+    /**
+     * Define the assoc array [name => type] for the data.
+     *
+     * @return array
+     */
+    abstract protected function getDefinition(): array;
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+}
---
+++ src/Omi/Parser/RareItemInfo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Kronthto\AOArchive\Omi\Parser;
+
+// Fixes
+class RareItemInfo extends AbstractParser
+{
+    protected function getDefinition(): array
+    {
+        return [
+            'id' => 'int',
+            'name' => 'string32', // 30 + padding
+            'ReqUseType' => 'int',
+            'ReqMinLevel' => 'int',
+            'ReqMaxLevel' => 'int',
+            'ReqItemKind' => 'short', // BYTE with padding
+            'ReqGearStat' => 'stat',
+            'DesParameter1' => 'byte',
+            'DesParameter2' => 'byte',
+            'DesParameter3' => 'byte',
+            'DesParameter4' => 'byte',
+            'DesParameter5' => 'byte',
+            'DesParameter6' => 'byte',
+            'DesParameter7' => 'byte',
+            'DesParameter8' => 'byte',
+            'DesParameter9' => 'byte',
+            'PaddingDesParameter' => 1,
+            'ParameterValue1' => 'float',
+            'ParameterValue2' => 'float',
+            'ParameterValue3' => 'float',
+            'ParameterValue4' => 'float',
+            'ParameterValue5' => 'float',
+            'ParameterValue6' => 'float',
+            'ParameterValue7' => 'float',
+            'ParameterValue8' => 'float',
+            'ParameterValue9' => 'float',
+            'probability' => 'int',
+        ];
+    }
+}
