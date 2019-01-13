4f2212713e2a5e28c0830910a89c9a959b00381c
Tobias Kronthaler <kronthto@gmail.com>
2019-01-13 15:16:06 +0100

    Restructure / Simplify Binary Writer pt1

--- src/Writer.php
+++ src/Writer.php
@@ -15,27 +15,38 @@ class Writer
         $this->stream = $stream;
     }
 
-    protected function makeBinary($value): string
+    public function write($value, int $length): void
     {
-        if (\is_int($value)) {
-            return decbin($value);
+        // Padding
+        if ($value === 0 || $value === null) {
+            for ($i = 0; $i < $length; ++$i) {
+                fwrite($this->stream, \chr(0));
+            }
+
+            return;
         }
+
+        // String with padding
         if (\is_string($value)) {
-            return base_convert(unpack('H*', $value)[1], 16, 2);
+            fwrite($this->stream, $value);
+            $this->write(0, $length - \strlen($value)); // Pad
+            return;
+        }
+
+        // Int with padding & endianess
+        if (\is_int($value)) {
+            // todo: this is Painful.. - maybe cleaner with pack / X? ......
+            // i feel like it should be possible without converting to bin stringm padding string null, and bindec again
+            $binaryPadded = str_pad(decbin($value), 8 * $length, '0', STR_PAD_LEFT);
+            for ($i = 0; $i < $length; ++$i) {
+                $byte = substr($binaryPadded, (($length - 1) - $i) * 8, 8);
+                fwrite($this->stream, \chr(bindec($byte)));
+            }
+
+            return;
         }
 
         throw new \BadMethodCallException('Not implemented type to convert to binary: ' . \gettype($value));
-    }
-
-    public function write($value, int $length): void
-    {
-        $changeEndian = \is_int($value);
-        // Painful..
-        $binaryPadded = str_pad($this->makeBinary($value), 8 * $length, '0', STR_PAD_LEFT);
-        for ($i = 0; $i < $length; ++$i) {
-            $byte = substr($binaryPadded, ($changeEndian ? (($length - 1) - $i) : $i) * 8, 8);
-            fwrite($this->stream, \chr(bindec($byte)));
-        }
     }
 
     public function writeRaw(string $bytes): void
