991e3ab13b02e328107030169f58a7bd3820b054
Tobias Kronthaler <kronthto@gmail.com>
2019-04-28 11:19:25 +0200

    Improve relevancy of fields in Omi items JSON data
    Certain keys are useless for certain item kinds, implemented that for CPUs
    
    Return canonical names for CPU stats

--- src/Omi/Parser/SmallItem.php
+++ src/Omi/Parser/SmallItem.php
@@ -4,6 +4,15 @@ namespace Kronthto\AOArchive\Omi\Parser;
 
 class SmallItem extends Item
 {
+    protected const CPUDESKEYS = [
+        1 => 'atk',
+        2 => 'def',
+        3 => 'fuel',
+        4 => 'spirit',
+        5 => 'shield',
+        6 => 'eva',
+    ];
+
     public function __construct(string $data)
     {
         parent::__construct($data);
@@ -47,5 +56,63 @@ class SmallItem extends Item
         $reducedData['DesParameters'] = $desParams;
 
         $this->data = $reducedData;
+    }
+
+    protected function getOnlyKeysByKind(): ?array
+    {
+        if ($this->data['kind'] === 26) {
+            return [
+                'id',
+                'kind',
+                'name',
+                'ReqUnitKind',
+                'ReqMinLevel',
+                'ReqMaxLevel',
+                'Weight',
+                'ItemAttribute',
+                'SourceIndex',
+                'description',
+                'DesParameters'
+            ];
+        }
+        return null;
+    }
+
+    public function jsonSerialize()
+    {
+        $data = parent::jsonSerialize();
+
+        $keys = $this->getOnlyKeysByKind();
+        if ($keys === null) {
+            $newData = &$data;
+        } else {
+            $newData = [];
+            foreach ($keys as $key) {
+                $newData[$key] = $data[$key];
+            }
+        }
+
+        $this->specialTransformByKind($newData);
+
+        return $newData;
+    }
+
+    protected function specialTransformByKind(&$arr): void
+    {
+        if ($this->data['kind'] === 26) {
+            $arr['CPUStats'] = $this->mapCPUDesParams($arr['DesParameters']);
+        }
+    }
+
+    protected function mapCPUDesParams(array $des): array
+    {
+        $new = [];
+        foreach ($des as $desKey => $value) {
+            if (!isset(static::CPUDESKEYS[$desKey])) {
+                continue;
+            }
+            $new[static::CPUDESKEYS[$desKey]] = $value;
+        }
+        return $new;
     }
 }
