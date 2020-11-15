8a1d09093e010dee6f0da53d61d4b6717b3befbd
Tobias Kronthaler <kronthto@gmail.com>
2020-11-15 11:40:34 +0100

    Adjust for OSR

--- src/Omi/Reader/TypeSizes.php
+++ src/Omi/Reader/TypeSizes.php
@@ -4,12 +4,12 @@ namespace Kronthto\AOArchive\Omi\Reader;
 
 abstract class TypeSizes
 {
-    public const ITEM = 472; // AR.de Client May2019 increased to 576
+    public const ITEM = 464; // AR.de Client May2019 increased to 576
     public const MONSTER = 136;
     public const MAPOBJECT = 12;
     public const RAREITEM = 112;
     public const BUILDINGNPC = 1100;
-    public const MAPINFO = 1248;
+    public const MAPINFO = 1188;
     public const LUCKYMACHINE = 228;
     public const MYSTERY_ITEM_DROP = 52;
     public const INVOKINGWEARITEM_DPNUM = 16;
@@ -19,7 +19,7 @@ abstract class TypeSizes
     public const PET_OPERATOR = 160;
     public const PET_LEVELDATA = 32;
     public const PET_BASEDATA = 64;
-    public const MIXING_INFO = 60;
+    public const MIXING_INFO = 56;
     public const DISSOLUTIONITEM = 52;
     public const CR_UK_18 = 12;
     public const CR_UK_19 = 30;
