9c3f70487654700706186e88bcefc3326043a397
Tobias Kronthaler <kronthto@gmail.com>
2022-11-16 22:03:50 +0100

    Linux compat

--- scripts/unpackArchive.php
+++ scripts/unpackArchive.php
@@ -4,12 +4,22 @@ require_once __DIR__ . '/_init.php';
 
 use Kronthto\AOArchive\Archive\Archive;
 
+ini_set('memory_limit', '500M');
+
 // Can be used for both, obj & eff Archives
 
+#$xorer = new \Kronthto\AOArchive\HExXorerCopy('XX');
 $arch = new Archive(file_get_contents($argv[1]), true);
 
-$outdir = sprintf('dec_%s', $argv[1]);
+#$arch = $arch->map(function (\Kronthto\AOArchive\Archive\ArchiveEntry $entry) use ($xorer): \Kronthto\AOArchive\Archive\ArchiveEntry {
+#    $entry->content = $xorer->doXor($entry->content);
+#
+#    return $entry;
+#});
+
+
+$outdir = sprintf('dec_%s', basename($argv[1]));
 
 mkdir($outdir);
 
-$arch->unpack("$outdir\\");
+$arch->unpack("$outdir".DIRECTORY_SEPARATOR);
